85860ab891e88c96a0322b60d7e238faea0ef30e
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-07-31 16:07:44 +0600

    Add Widget Section to Admin Dashboard

---
+++ client/src/components/specific/Charts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Line, Doughnut } from "react-chartjs-2";
+
+const LineCharts = () => {
+  return <div>Charts</div>;
+};
+const DoughnutCharts = () => {
+  return <div>Charts</div>;
+};
+
+export { LineCharts, DoughnutCharts };
--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
+
 import { Box, Container, Paper, Stack, Typography } from "@mui/material";
 import {
   AdminPanelSettings as AdminPanelSettingsIcon,
+  Group as GroupIcon,
+  Message as MessageIcon,
   NotificationAdd as NotificationIcon,
+  Person as PersonIcon,
 } from "@mui/icons-material";
 import moment from "moment";
+
 import {
   CurveButton,
   SearchBox,
 } from "../../components/styles/StyledComponents";
+import { matblack } from "../../constants/color";
+import { DoughnutCharts, LineCharts } from "../../components/specific/Charts";
 
 const Dashboard = () => {
   const appbar = (
@@ -38,16 +45,100 @@ const Dashboard = () => {
     </Paper>
   );
 
-  const widgets = <></>;
+  const widgets = (
+    <Stack
+      direction={{ xs: "column", sm: "row" }}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      margin={"2rem 0"}
+      spacing={"2rem"}
+    >
+      <Widget title="Users" value="100" icon={<PersonIcon />} />
+      <Widget title="Chats" value="50" icon={<GroupIcon />} />
+      <Widget title="Messages" value="35" icon={<MessageIcon />} />
+    </Stack>
+  );
   return (
     <AdminLayout>
       <Container component={"main"}>
         {appbar}
-        <Stack>char area</Stack>
+        <Stack direction={"row"} spacing={"2rem"} flexWrap={"wrap"}>
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "2rem 3.5rem",
+              borderRadius: "1rem",
+              width: "100%",
+              maxWidth: "45rem",
+            }}
+          >
+            <Typography>Last Massages</Typography>
+            <LineCharts />
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "1rem",
+              borderRadius: "1rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: { xs: "100%", sm: "50%" },
+              position: "relative",
+              width: "100%",
+              maxWidth: "25rem",
+              height: "25rem",
+            }}
+          >
+            <DoughnutCharts />
+            <Stack
+              position={"absolute"}
+              direction={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              spacing={"0.5rem"}
+              width={"100%"}
+              height={"100%"}
+            >
+              <GroupIcon /> <Typography>Vs</Typography> <PersonIcon />
+            </Stack>
+          </Paper>
+        </Stack>
         {widgets}
       </Container>
     </AdminLayout>
   );
 };
+
+const Widget = ({ title, value, icon }) => (
+  <Paper
+    elevation={3}
+    sx={{
+      padding: "2rem",
+      margin: "2rem 0",
+      borderRadius: "1.5rem",
+      width: "20rem",
+    }}
+  >
+    <Stack alignItems={"center"} spacing={"1rem"}>
+      <Typography>{title}</Typography>
+      {icon}
+      <Typography
+        sx={{
+          color: "rgba(0,0,0,0.7)",
+          borderRadius: "50%",
+          border: `5px solid ${matblack}`,
+          width: "5rem",
+          height: "5rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {value}
+      </Typography>
+    </Stack>
+  </Paper>
+);
 
 export default Dashboard;
