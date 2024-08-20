6d4094c8e746a551836359f67fc6f1598d9bec1c
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-08-20 17:31:59 +0600

    setup socket.io

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { connectDB } from "./utils/featurs.js";
-import dotenv from "dotenv";
-import { errrorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { connectDB } from "./utils/featurs.js";
+import { errrorMiddleware } from "./middlewares/error.js";
+import { v4 as uuid } from "uuid";
+
+import { Server } from "socket.io";
+import { createServer } from "http";
 dotenv.config({
   path: "./.env",
 });
@@ -10,14 +14,21 @@ dotenv.config({
 import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
 import amninRoutes from "./routes/admin.routes.js";
+import { NEW_MASSAGE, NEW_MASSAGES } from "./constants/event.js";
+import { getSockets } from "./lib/helper.js";
+import { Message } from "./models/massages.model.js";
+import { create } from "domain";
 
 const mongoUrI = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
+const userSocketIDs = new Map();
 
 // connect to database
 connectDB(mongoUrI);
 // create express app
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {});
 
 // using middleware
 app.use(express.json());
@@ -31,8 +42,65 @@ app.use("/admin", amninRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//  connect to socket.io
+io.use((socket, next) => {
+  
+});
+io.on("connection", (socket) => {
+  const user = {
+    _id: "sjhdshdjk",
+    name: "parves",
+  };
+  userSocketIDs.set(user._id.toString(), socket.id);
+  console.log(userSocketIDs);
+
+  socket.on(NEW_MASSAGES, async ({ chatId, members, message }) => {
+    const realTimeMessage = {
+      _id: uuid(),
+      content: message,
+      sender: {
+        _id: user._id,
+        name: user.name,
+      },
+      chatId,
+      createdAt: new Date().toISOString(),
+    };
+
+    const messageForDb = {
+      content: message,
+      chatId,
+      sender: user._id,
+    };
+    const membersSockets = getSockets(members);
+
+    io.to(membersSockets).emit(NEW_MASSAGES, {
+      message: realTimeMessage,
+      chatId,
+    });
+
+    // messages alert
+    io.to(membersSockets).emit(NEW_MASSAGE, { chatId });
+
+    try {
+      await Message.create(messageForDb);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    userSocketIDs.delete(user._id.toString());
+  });
+});
+
 // error middleware
 app.use(errrorMiddleware);
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} in ${process.env.NODE_ENV} mode`);
+server.listen(port, () => {
+  console.log(
+    `Server is running on port ${port} in ${process.env.NODE_ENV} mode`
+  );
 });
+
+export { userSocketIDs };
--- server/lib/helper.js
+++ server/lib/helper.js
@@ -1,3 +1,9 @@
+import { userSocketIDs } from "../app.js";
+
 export const getOtherMembers = (members, userId) => {
   return members.find((member) => member._id.toString() !== userId.toString());
+};
+
+export const getSockets = (users = []) => {
+  return users.map((user) => userSocketIDs.get(user._id.toString()));
 };
